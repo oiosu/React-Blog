0223e7797f054312a222a90493f8589e4e0b8158
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-09-26 15:27:12 +0900

    JSX 문법 3가지

--- 01_블로그제작&기초문법/blog/src/App.js
+++ 01_블로그제작&기초문법/blog/src/App.js
@@ -2,22 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let post = '광화문 우동 맛집';
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>역시 setting up 작업은 시간이 오래걸린다</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <h4>BLOG</h4>
+      </div>
+      <h4>{post}</h4>
     </div>
   );
 }
