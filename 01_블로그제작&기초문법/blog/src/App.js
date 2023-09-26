683199d2af7d31522949bb872454a4ea7cbf65c9
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-09-26 16:19:04 +0900

    useState 숙제 완료

--- 01_블로그제작&기초문법/blog/src/App.js
+++ 01_블로그제작&기초문법/blog/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  let [Title1, blogTitle] = useState('Title_1')
 
   return (
     <div className="App">
@@ -10,7 +11,15 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
       <div className='list'>
-        <h4>Title_1</h4>
+        <h4>{Title1}</h4>
+        <p>9월 26일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>.</h4>
+        <p>9월 26일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>.</h4>
         <p>9월 26일 발행</p>
       </div>
     </div>
