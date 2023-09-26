1fa89c65ed6ddd5a9629174a232ac4821e123bc9
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-09-26 15:58:45 +0900

    useState 언제 사용하면 좋을까

--- 01_블로그제작&기초문법/blog/src/App.js
+++ 01_블로그제작&기초문법/blog/src/App.js
@@ -4,13 +4,10 @@ import { useState } from 'react';
 
 function App() {
 
-  let post = '광화문 우동 맛집';
-  let [Title, b] = useState('남자 가디건 추천')
-
   return (
     <div className="App">
       <div className='black-nav'>
-        <h4>Su kyung BLOG</h4>
+        <h4>ReactBlog</h4>
       </div>
       <div className='list'>
         <h4>{Title}</h4>
